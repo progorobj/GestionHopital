75abb1fd93fe75e50a94a281690f66f3df178b57
Carlos <carlos@nom.com>
2021-08-24 13:48:25 -0400

    Changements

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -29,8 +29,6 @@ namespace GestionHopital
         string pass;
         private void btnValider_Click(object sender, RoutedEventArgs e)
         {
-            string leNom = "";
-
             nomU = txtUtilisateur.Text;
             pass = txtPass.Text;
             if (String.IsNullOrEmpty(nomU) &&
